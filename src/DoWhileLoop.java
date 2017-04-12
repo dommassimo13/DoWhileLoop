e865b959cad7f1463ac090a164164f0a58b4e966
dominic massimo <dominicmassimo@dominics-iMac.local>
2017-04-12 08:03:00 -0400

    Initial commit

---
+++ src/DoWhileLoop.java
@@ -0,0 +1,12 @@
+/**
+ * Created by dominicmassimo on 4/11/17.
+ */
+public class DoWhileLoop {
+    public static void main(String args[]){
+        int i=100;
+        do{
+            System.out.println(i);
+            i--;
+        }while(i>1);
+    }
+}
